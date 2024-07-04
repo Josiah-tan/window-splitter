4c3d7181c4d1168361d2b359c323f82c0c075a04
Josiah-tan <josiah123t@gmail.com>
2024-07-04 20:24:33 +1000

    commented out open cv related things

--- sender.py
+++ sender.py
@@ -63,6 +63,6 @@ if response.status_code == 200:
 else:
     print("Failed to send data")
 
-cv2.imshow("image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("image", image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
